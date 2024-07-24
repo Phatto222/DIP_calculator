7a8812c5ca86ba0c0ef3b90a431b2c602b5f508b
Phatto222 <phat.ph2007@gmail.com>
2024-07-24 09:54:19 +0700

    add button

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ class MyCalculator:
 
         self.root = tk.Tk()
 
-        self.root.geometry("380x380")
+        self.root.geometry("300x400")
         self.root.title("My Calculator")
 
-        self.label = tk.Label(self.root, text="Hello DIP02", font=('Arial', 18))
+        self.label = tk.Label(self.root, text="Calculator", font=('Arial', 18))
         self.label.pack()
         
-        self.button = tk.Button(self.root, text="Click Here", height=4)
-        self.button.place(x=20, y=50)
+        self.button = tk.Button(self.root, text="1", width=6, height=6)
+        self.button.place(x=20, y=80)
+        
+        self.button = tk.Button(self.root, text="Don't click", height=4)
+        self.button.place(x=100, y=80)
+
+        self.button = tk.Entry()
+        self.button.place(x=80, y=40)
 
         self.root.mainloop()
 
-MyCalculator()
+MyCalculator() 
