21112ff69215f4ce3a57ac7c3dc121faecababcd
Phatto222 <phat.ph2007@gmail.com>
2024-08-14 08:25:53 +0700

    num add

--- main.py
+++ main.py
@@ -11,14 +11,32 @@ class MyCalculator:
         self.label = tk.Label(self.root, text="Calculator", font=('Arial', 18))
         self.label.pack()
         
-        self.button = tk.Button(self.root, text="1", width=6, height=6)
+        self.button = tk.Button(self.root, text="1", width=8, height=4)
         self.button.place(x=20, y=80)
         
-        self.button = tk.Button(self.root, text="Don't click", height=4)
-        self.button.place(x=100, y=80)
+        self.button = tk.Button(self.root, text="2", width=8, height=4)
+        self.button.place(x=90, y=80)
 
-        self.button = tk.Entry()
-        self.button.place(x=80, y=40)
+        self.button = tk.Button(self.root, text="3", width=8, height=4)
+        self.button.place(x=160, y=80)
+
+        self.button = tk.Button(self.root, text="4", width=8, height=4)
+        self.button.place(x=20, y=160)
+
+        self.button = tk.Button(self.root, text="5", width=8, height=4)
+        self.button.place(x=90, y=160)
+
+        self.button = tk.Button(self.root, text="6", width=8, height=4)
+        self.button.place(x=160, y=160)
+
+        self.button = tk.Button(self.root, text="7", width=8, height=4)
+        self.button.place(x=20, y=240)
+
+        self.button = tk.Button(self.root, text="8", width=8, height=4)
+        self.button.place(x=90, y=240)
+
+        self.button = tk.Button(self.root, text="9", width=8, height=4)
+        self.button.place(x=160, y=240)
 
         self.root.mainloop()
 
